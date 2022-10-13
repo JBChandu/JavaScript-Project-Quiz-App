f0f529f45c2d9b11705c79a9deca3b3ee3298032
chcodes <chandrahassan15@gmail.com>
2022-10-13 07:49:21 +0530

    Commited with Latest Code

--- script.js
+++ script.js
@@ -37,12 +37,12 @@ const quizData = [
 ];
 
 const quiz = document.querySelector("#quiz");
-const answerEls = [];
+const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.querySelector("#question");
-const a_text = document.querySelector("#a");
-const b_text = document.querySelector("#b");
-const c_text = document.querySelector("#c");
-const d_text = document.querySelector("#d");
+const a_text = document.querySelector("#a_text");
+const b_text = document.querySelector("#b_text");
+const c_text = document.querySelector("#c_text");
+const d_text = document.querySelector("#d_text");
 const submitBtn = document.querySelector("#submit");
 
 let currentQuiz = 0;
@@ -51,18 +51,63 @@ let score = 0;
 loadQuiz();
 
 function loadQuiz() {
-  for (let i = 0; i < quizData.length; ) {
-    questionEl.textContent = quizData[i].question;
-    a_text.textContent = quizData[i].a;
-    console.log(quizData[i].a);
-    b_text.textContent = quizData[i].b;
-    c_text.textContent = quizData[i].c;
-    d_text.textContent = quizData[i].d;
-  }
+  questionEl.textContent = quizData[currentQuiz].question;
+  a_text.textContent = quizData[currentQuiz].a;
+  b_text.textContent = quizData[currentQuiz].b;
+  c_text.textContent = quizData[currentQuiz].c;
+  d_text.textContent = quizData[currentQuiz].d;
 }
 
-function deselectAnswers() {}
+function deselectAnswers() {
+  answerEls.forEach((data) => {
+    data.checked = false;
+  });
+}
 
-function getSelected() {}
+function getSelected() {
+  let selec = "";
 
-submitBtn.addEventListener("click", () => {});
+  answerEls.forEach((data) => {
+    if (data.checked === true) {
+      selec = data.id;
+    }
+  });
+  return selec;
+}
+
+submitBtn.addEventListener("click", () => {
+  let selectedAns = getSelected();
+  // if none of the options is selected give a alert do a reload
+  if (!selectedAns) {
+    alert("Please select one option");
+    window.location.reload();
+  }
+  // if answer is selected check with the quizdata object
+  if (selectedAns === quizData[currentQuiz].correct) {
+    score += 1;
+    console.log(score);
+  }
+  if (currentQuiz < quizData.length - 1) {
+    //
+    currentQuiz += 1;
+    deselectAnswers();
+    loadQuiz();
+  } else {
+    // alert(`Your score is :${score} You can retake the quiz if Needed`);
+    quiz.innerHTML = `Your Score is: ${score} / ${quizData.length}`;
+    quiz.style.textAlign = "center";
+
+    // create a retry button
+    let retry = document.createElement("button");
+    retry.textContent = "RE-TRY";
+    quiz.append(retry);
+    currentQuiz = 0;
+    score = 0;
+    loadQuiz();
+    deselectAnswers();
+    // retry button click
+    retry.addEventListener("click", () => {
+      window.location.reload();
+    });
+  }
+});
